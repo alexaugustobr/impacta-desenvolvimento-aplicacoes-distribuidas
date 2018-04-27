e55c690ac368e905339318b5f78dacd33d166010
Fabio-ADS2 <fabio@amazestudio.com.br>
2018-04-26 21:45:03 -0300

    correçao

--- Services/ForumActivate.py
+++ Services/ForumActivate.py
@@ -1,11 +1,19 @@
 from Server import forums
+from Services.ConsultaForum import consultarForum
 
 def AtivarForum():
     Dados = request.get_json()
 
-    for forum in forums:
-        if str(forum["ForumId"]) == str(Dados["ForumId"]) and str(forum["OwnerId"]) == str(Dados["OwnerId"]):
-            if forum["Active"] == False:
-                forum["Active"] = True
-                return forum
-    return None
+    forum = consultarForum(Dados["ForumId"])
+    if forum:
+        raise Exception("Forum nao encontrado")
+
+    if str(forum["OwnerId"]) != str( Dados["OwnerId"]):
+        raise Exception("OwnerId nao é o mesmo que o ownerID do Forum")
+    
+    if forum["Active"] == True:
+        raise Exception("Forum ja esta ativo")
+
+    forum["Active"] = True
+
+    return forum
