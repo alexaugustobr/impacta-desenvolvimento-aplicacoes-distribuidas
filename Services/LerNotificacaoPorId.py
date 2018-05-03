44c8993989e6004f5098abfc6bca2b42fb31633e
henrique221 <hborges9294@gmail.com>
2018-05-03 20:06:58 -0300

    ler notificacaoe por id

---
+++ Services/LerNotificacaoPorId.py
@@ -0,0 +1,8 @@
+from Server import notificacoes
+from Models import Notificacao
+
+def lerNotificacaoPorRa(notificacaoId):
+    for notificacao in notificacoes:
+        if notificacoes["id"] == notificacoesId:
+            return notificacao
+    return None
