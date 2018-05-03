7df6b18e522adf75d5b1e78548cdcdc1d75c56a0
michaelads <maike_maicon09@hotmail.com>
2018-05-03 20:35:12 -0300

    arquivaar

--- Services/ArquivarNotificacao.py
+++ Services/ArquivarNotificacao.py
@@ -1,11 +1,17 @@
 from Server import notificacoes
 from Models.Notificacao import Notificacao
+from Services.LerNotificacaoPorId import lerNotificacaoPorRa
+from Services.LerNotificacaoPorRa import lerNotificacaoPorRa
 
-def arquivarNotificacao(){
-    for notificacao in notificacoes:
-        if notificacao["status"] == "Nao vizualizado":
-            notificacao["status"] = "Vizualizado"
+def arquivarNotificacao(ra, id){
+    arquivarId = lerNotificacaoPorId(id)
+    arquivarRa = lerNotificacaoPorRa(ra)
+
+    if arquivarRa and arquivarId:
+        notificacoes["status"] = "Arquivada"
+
+        return notificacoes
             
-    return notificacao
+    return None
 
 }
